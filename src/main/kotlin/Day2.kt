829727e31f2b804c0632ab682e6be07b4fecf9b5
Merlin <30577208+SimplyMerlin@users.noreply.github.com>
2022-12-02 11:49:18 +0100

    Day 2-B

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -2,8 +2,12 @@ import java.io.File
 import java.io.InputStream
 
 fun main() {
-    val inputStream: InputStream = File("src/main/resources/day2.txt").inputStream()
+    a()
+    b()
+}
 
+fun a() {
+    val inputStream: InputStream = File("src/main/resources/day2.txt").inputStream()
     var points = 0
     inputStream.bufferedReader().forEachLine {
         val choice = it.split(" ")
@@ -12,7 +16,6 @@ fun main() {
             "Y" -> points += 2
             "Z" -> points += 3
         }
-        println(choice)
         if ((choice[0] == "A" && choice[1] == "Y") || (choice[0] == "B" && choice[1] == "Z") || (choice[0] == "C" && choice[1] == "X")) {
             points += 6
             return@forEachLine
@@ -24,6 +27,41 @@ fun main() {
         points += 3
     }
 
-    println(points)
+    println("A: $points")
+}
 
+fun b() {
+    val inputStream: InputStream = File("src/main/resources/day2.txt").inputStream()
+    var points = 0
+    inputStream.bufferedReader().forEachLine {
+        val choice = it.split(" ")
+        when(choice[1]) {
+            "X" -> {
+                points += 0
+                when(choice[0]) {
+                    "A" -> points += 3
+                    "B" -> points += 1
+                    "C" -> points += 2
+                }
+            }
+            "Y" -> {
+                points += 3
+                when(choice[0]) {
+                    "A" -> points += 1
+                    "B" -> points += 2
+                    "C" -> points += 3
+                }
+            }
+            "Z" -> {
+                points += 6
+                when(choice[0]) {
+                    "A" -> points += 2
+                    "B" -> points += 3
+                    "C" -> points += 1
+                }
+            }
+        }
+    }
+
+    println("B: $points")
 }
